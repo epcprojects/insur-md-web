f1657a7c8ccfd1a06dc0dec2ec68485fe7058a3e
epcprojects <endpointclients@gmail.com>
2026-03-26 23:45:22 +0500

    new pages added and header footer fixes.

---
+++ app/(pages)/security-compliance/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className=" min-h-dvh p-4">
+      <div className="rounded-xl bg-[#F5F5F5] w-full h-40"></div>
+    </div>
+  );
+};
+
+export default Page;
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,6 +1,5 @@
 import { ThemeButton } from "./buttons/ThemeButton";
 import HeroSection from "./sections/HeroSection";
 import Header from "./layout/header";
-export {ThemeButton,HeroSection ,Header}
-
-
+import Footer from "./layout/Footer";
+export { ThemeButton, HeroSection, Header, Footer };
